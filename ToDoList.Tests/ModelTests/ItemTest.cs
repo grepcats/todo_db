22d9e50b19966f0905d11196488c8a65b6b8e344
Joel Adams and Kayla Ondracek <joel@>
2018-02-20 09:11:58 -0800

    add red test for save method

--- ToDoList.Tests/ModelTests/ItemTest.cs
+++ ToDoList.Tests/ModelTests/ItemTest.cs
@@ -6,12 +6,13 @@ using System;
 namespace ToDoList.Tests
 {
   [TestClass]
-  public class ItemTests
+  public class ItemTests : IDisposable
   {
-    // public void Dispose()
-    // {
-    //   Item.DeleteAll();
-    // }
+    public void Dispose()
+    {
+      Item.DeleteAll();
+    }
+
     public ItemTests()
     {
       DBConfiguration.ConnectionString = "server=localhost;user id=root;password=root;port=8889;database=todo_test;";
@@ -38,6 +39,19 @@ namespace ToDoList.Tests
       int result = Item.GetAll().Count;
 
       //Assert
+      Assert.AreEqual(0, result);
+    }
+
+    [TestMethod]
+    public void Save_SavesToDatabase_ItemList()
+    {
+      //arrange
+      Item testItem = new Item("Go to store");
+
+      //act
+      int result = Item.GetAll().Count;
+
+      //assert
       Assert.AreEqual(0, result);
     }
   }
